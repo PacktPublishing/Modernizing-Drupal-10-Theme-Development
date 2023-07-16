9e7a418414b4daff57bc7a7e69746b889ebd1bba
Luca Lusso <lussoluca@gmail.com>
2023-07-16 23:09:20 +0200

    More SDC

--- web/themes/custom/alps_trips/components/at-button/at-button.js
+++ web/themes/custom/alps_trips/components/at-button/at-button.js
@@ -1,15 +1,15 @@
-// eslint-disable-next-line func-names
 (function (Drupal, once) {
   Drupal.behaviors.at_button = {
     attach(context) {
-      const [button] = once('at-button', '.at-button', context);
-      if (button) {
-        button.addEventListener('click', function (event) {
-          event.preventDefault();
-          alert('Hello World!');
+      once('at-button', '.at-button', context)
+        .forEach((item) => {
+          item.addEventListener(
+            'click',
+            function (event) {
+              event.preventDefault();
+              alert('Hello World!');
+          });
         });
-      }
     },
   };
-  // eslint-disable-next-line no-undef
 })(Drupal, once);
